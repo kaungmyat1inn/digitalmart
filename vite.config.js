54f0b4604f9cd24cfcc5b54ac714fd99f1f0133f
kaungmyat1inn <kaungmyatlin90@gmail.com>
2026-01-24 22:28:27 +0630

    hot reload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "0.0.0.0", // Network ထဲက စက်အားလုံး ဝင်ကြည့်ခွင့်ပေးတာပါ
+        hmr: {
+            host: "192.168.100.62", // စောစောက ရလာတဲ့ သင့် Mac ရဲ့ IP ကို ဒီမှာ ထည့်ပါ
+        },
+    },
 });
